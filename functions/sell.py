ea1de461753be90ab9628efc3cadd5ef7e8b942a
Martin op iMac <wolk9@me.com>
2023-10-04 11:46:08 +0000

    bug-fix sell

--- functions/sell.py
+++ functions/sell.py
@@ -12,55 +12,63 @@ from functions.inventory import get_next_id
 
 
 def sell_product(product_name, price, quantity=1):
-    price = round(float(price),2)
+    # Round the price to two decimal places
+    price = round(float(price), 2)
+
+    # Get today's date
     today = get_today()
-    with open(INVENTORY_FILE, "r", newline="") as inventory_file, \
-            open(SOLD_FILE, "a", newline="") as sold_file:
+
+    # Read the inventory from the CSV file
+    with open(INVENTORY_FILE, "r", newline="") as inventory_file:
         inventory_reader = csv.reader(inventory_file)
-        sold_writer = csv.writer(sold_file)
+        inventory = list(inventory_reader)[1:]
 
-        # Find all products with the same name that are in stock
-        products = []
-        for row in inventory_reader:
-            if row[1] == product_name and int(float(row[3])) > 0:
-                row[3] = round(float(row[3]), 2)  # convert buy price to float
-                row[4] = datetime.datetime.strptime(row[4], "%Y-%m-%d").date()  # convert expiration date to date object
-                products.append(row)
+    # Find all products with the same name that are in stock
+    products = []
+    for row in inventory:
+        if row[1] == product_name and int(float(row[3])) > 0:
+            row[3] = round(float(row[3]), 2)  # round buy price to 2 decimal places
+            row[4] = datetime.datetime.strptime(row[4], "%Y-%m-%d").date()  # convert expiration date to date object
+            products.append(row)
 
-        # Sort products by nearest expiring date and lowest buyprice
-        products.sort(key=lambda x: (x[4], x[2]))
+    # Sort products by nearest expiring date and lowest buy price
+    products.sort(key=lambda x: (x[4], x[2]))
 
-        # Check if there are enough products in stock
-        total_quantity = sum(int(float(product[3])) for product in products)
-        if total_quantity < quantity:
-            print(f"Not enough {product_name}(s) in stock")
-            if total_quantity > 0:
-                answer = input(f"Do you want to sell the remaining {total_quantity} {product_name}(s)? (Yes/No) ")
-                if answer.lower() == "yes":
-                    quantity = total_quantity
-                else:
-                    return
+    # Check if there are enough products in stock
+    total_quantity = sum(int(float(product[3])) for product in products)
+    if total_quantity < quantity:
+        print(f"Not enough {product_name}(s) in stock")
+        if total_quantity > 0:
+            answer = input(f"Do you want to sell the remaining {total_quantity} {product_name}(s)? (Yes/No) ")
+            if answer.lower() == "yes":
+                quantity = total_quantity
             else:
                 return
-        with open(INVENTORY_FILE, "w", newline="") as inventory_file:
-            inventory_writer = csv.writer(inventory_file)
-            inventory_writer.writerow(INVENTORY_HEADER)
-        # Sell the products and add a record to the sold file
-            for product in products:
-                if quantity == 0:
-                    break
-                if int(float(product[3])) >= quantity:
-                    sold_quantity = quantity
-                else:
-                    sold_quantity = int(float(product[3]))
-                product_id = product[0]
-                sold_id = get_next_id(SOLD_FILE)
-                sold_row = [sold_id, product_id, product_name, today.strftime("%Y-%m-%d"),
-                            round(float(price), 2), sold_quantity]
-                sold_writer.writerow(sold_row)
-                product[3] = str(round(float(product[3]),2) - sold_quantity)
+        else:
+            return
 
-                # Update the inventory file
-                inventory_writer.writerows(products)
+    # Sell the products and update the inventory
+    with open(INVENTORY_FILE, "w", newline="") as inventory_file, \
+            open(SOLD_FILE, "a", newline="") as sold_file:
+        inventory_writer = csv.writer(inventory_file)
+        inventory_writer.writerow(INVENTORY_HEADER)
+        sold_writer = csv.writer(sold_file)
+        for product in products:
+            if quantity == 0:
+                break
+            if int(float(product[3])) >= quantity:
+                sold_quantity = quantity
+            else:
+                sold_quantity = int(float(product[3]))
+            product_id = product[0]
+            sold_id = get_next_id(SOLD_FILE)
+            sold_row = [sold_id, product_id, product_name, today.strftime("%Y-%m-%d"),
+                        round(float(price), 2), sold_quantity]
+            sold_writer.writerow(sold_row)
+            product[3] = str(round(float(product[3]), 2) - sold_quantity)
+            inventory_writer.writerow(product)
 
-        print(f"Sold {sold_quantity} {product_name}('s)")
+            # Update the inventory file
+            inventory_writer.writerows(products)
+
+    print(f"Sold {sold_quantity} {product_name}(s)")
